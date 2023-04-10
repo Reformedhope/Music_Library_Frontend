987ba96cee14214cfb013708d93f667b57a9dd26
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-10 14:34:40 -0400

    Created a table and pulled the backend info into the table

--- src/App.js
+++ src/App.js
@@ -19,11 +19,42 @@ function App() {
 
 
   return (
-    <div>
-      <button onClick={()=> getAllSongs }>Get All Songs</button>
+    // <div>
+    //   <button onClick={()=> getAllSongs }>Get All Songs</button>
 
-    </div>
+    // </div>
+    <div>
+    <table>
+        <thead>
+            <tr>
+               <th>Title</th>
+               <th>Artist</th>
+               <th>Album</th>
+               <th>Release Date</th>
+               <th>Genre</th>
+               <th>Likes</th>
+            </tr>
+        </thead>
+        <tbody>
+          {songs.map((song)=>{
+            return(
+              <tr>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.release_date}</td>
+                <td>{song.genre}</td>
+                <td>{song.likes}</td>
+              </tr>
+
+            );
+          })}
+        </tbody>
+    </table>
+</div>
   );
+
+  
 }
 
 export default App;
