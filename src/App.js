e7aaa47cc2c70373490e55fc3bd934dae1fe745e
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-10 12:26:29 -0400

    cretaed Async function for get all songs + added useeffect and created an on click button to getallsongs

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';   
+import axios from 'axios';
+
+
+function App() {
+
+  const[songs, setSongs] =useState([]);
+  
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/songs/');
+    console.log(response.data);
+    setSongs(response.data)
+  };
+
+
+  return (
+    <div>
+      <button onClick={()=> getAllSongs }>Get All Songs</button>
+
+    </div>
+  );
+}
+
+export default App;
